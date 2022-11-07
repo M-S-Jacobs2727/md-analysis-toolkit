41e5f118fc8268a91b12a89d1b43ddf24d210d82
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-11-07 13:39:41 -0500

    added functionality for COM MSD

--- msd.jl
+++ msd.jl
@@ -13,8 +13,14 @@ in each file. If false (default), they are sorted by the column 'id'.
 If types == (0,) (default), the MSD for each particle is averaged. If
 types is given and is a tuple of ints >= 1, then those types are 
 independently accounted for, and an overall MSD is given as well.
+
+If mol is set to true, then the molecule property must be available in
+the dump file. The center of mass of each molecule will be analyzed 
+instead of each atom. This assumes that the mass of each atom type is
+1. If not, then set the masses keyword to a vector where each element
+mass[i] is the mass of atoms of type i.
 """
-function msd(filenames...; max_num_frames=100, sorted=false, types=(0,))
+function msd(filenames...; max_num_frames=100, sorted=false, types=(0,), mol=false, masses=[1,])
     for f in filenames
         frame = LammpsFiles.read_dump(f)
         inds = [findfirst(x->x==s, frame.properties) for s in ["xu", "yu", "zu"]]
