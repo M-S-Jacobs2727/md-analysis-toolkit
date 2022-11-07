29709d045bbc9ce5d94a9448b348dd4e538717f9
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-11-07 14:43:04 -0500

    implemented msd by type

--- msd.jl
+++ msd.jl
@@ -9,39 +9,65 @@ import LammpsFiles
     Computes the MSD for a collection of LAMMPS dump files. The range of
     increments is from every 1 snapshot to every 'max_num_frames' 
     snapshots (100 by default). 
-    
+
     If sorted == true, then the atoms are assumed to be in the same order
     in each file. If false (default), they are sorted by the column 'id'.
-    
+
     [Not implemented]
-    If types == (0,) (default), the MSD for each particle is averaged. If
-    types is given and is a tuple of ints >= 1, then those types are 
-    independently accounted for, and an overall MSD is given as well.
-    
+    If types is false (default), the MSD for each particle is averaged. If
+    types is true, then each type is independently evaluated, and an 
+    overall MSD is given as well. Types are only checked for the first dump
+    file, so if atoms change type, that will invalidate the calculation.
+
     If mol is set to true, then the molecule property must be available in
     the dump file. The center of mass of each molecule will be analyzed 
     instead of each atom. This assumes that the mass of each atom type is
     1. If not, then set the masses keyword to a vector where each element
     mass[i] is the mass of atoms of type i.
 """
-function msd(filenames...; max_num_frames=100, sorted=false, types=(0,), mol=false, masses=[1,])
+function msd(filenames...; max_num_frames=100, sorted=false, by_type=false, mol=false, masses=nothing)
     frame = LammpsFiles.read_dump(filenames[1])
-    all_coords = zeros(3, frame.natoms, length(filenames))
-    for (i, f) in enumerate(filenames)
-        frame = LammpsFiles.read_dump(f)
-        inds = [findfirst(x->x==s, frame.properties) for s in ["xu", "yu", "zu"]]
-        coords = frame.atoms[inds, : ]
-        if !sorted
-            sort_inds = sortperm(frame.atoms[findfirst(x->x=="id", frame.properties), : ])
-            coords = coords[:, sort_inds]
-        end
-        all_coords[ : , : , i] = coords
+    id_col = findfirst(x->x=="id", frame.properties)
+    type_col = findfirst(x->x=='type', frame.properties)
+    coord_cols = [findfirst(x->x==s, frame.properties) for s in ["xu", "yu", "zu"]]
+
+    coords = frame.atoms[coord_cols, : ]
+    if !sorted
+        sort_inds = sortperm(frame.atoms[id_col, : ])
+        coords = coords[:, sort_inds]
     end
 
-    msd = zeros(max_num_frames)
+    all_coords = zeros(3, frame.natoms, length(filenames))
+    all_coords[ : , : , 1] = coords
+
+    if by_type
+        types = unique(frame.atoms[type_col, : ])
+        selections = [findall(x->round(x)==t, frame.atoms[type_col, : ]) for t in types]
+    else
+        types = [0, ]
+    end
+        
+
+
+    for (i, f) in enumerate(filenames[2:end])
+        frame = LammpsFiles.read_dump(f)
+        coords = frame.atoms[coord_cols, : ]
+        if !sorted
+            sort_inds = sortperm(frame.atoms[id_col, : ])
+            coords = coords[:, sort_inds]
+        end
+        all_coords[ : , : , i+1] = coords[ : , selection]
+    end
+
+    msd = zeros(length(types), max_num_frames)
     for dt=1:max_num_frames
         diff = all_coords[ : , : , dt+1:end] - all_coords[ : , : , 1:end-dt]
-        msd[dt] = mean(sum(diff .* diff, dims=1))
+        msd[1, dt] = mean(sum(diff .* diff, dims=1))
+        if by_type
+            for (i, sel) in selections
+                msd[i+1, dt] = mean(sum(diff[ : , sel, : ] .* diff[ : , sel, : ], dims=1))
+            end
+        end
     end
     return msd
 end
