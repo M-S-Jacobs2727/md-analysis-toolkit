bac9e2caa07b126a56c7342b4e4db9e65d3d2767
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-11-07 13:58:33 -0500

    updated msd docstring

--- msd.jl
+++ msd.jl
@@ -3,24 +3,26 @@ using Statistics
 import LammpsFiles
 
 """
-[Implemented]
-Computes the MSD for a collection of LAMMPS dump files. The range of
-increments is from every 1 snapshot to every 'max_num_frames' 
-snapshots (100 by default). 
+    msd(filenames...; max_num_frames=100, sorted=false, types=(0,), mol=false, masses=[1,])
 
-If sorted == true, then the atoms are assumed to be in the same order
-in each file. If false (default), they are sorted by the column 'id'.
-
-[Not implemented]
-If types == (0,) (default), the MSD for each particle is averaged. If
-types is given and is a tuple of ints >= 1, then those types are 
-independently accounted for, and an overall MSD is given as well.
-
-If mol is set to true, then the molecule property must be available in
-the dump file. The center of mass of each molecule will be analyzed 
-instead of each atom. This assumes that the mass of each atom type is
-1. If not, then set the masses keyword to a vector where each element
-mass[i] is the mass of atoms of type i.
+    [Implemented]
+    Computes the MSD for a collection of LAMMPS dump files. The range of
+    increments is from every 1 snapshot to every 'max_num_frames' 
+    snapshots (100 by default). 
+    
+    If sorted == true, then the atoms are assumed to be in the same order
+    in each file. If false (default), they are sorted by the column 'id'.
+    
+    [Not implemented]
+    If types == (0,) (default), the MSD for each particle is averaged. If
+    types is given and is a tuple of ints >= 1, then those types are 
+    independently accounted for, and an overall MSD is given as well.
+    
+    If mol is set to true, then the molecule property must be available in
+    the dump file. The center of mass of each molecule will be analyzed 
+    instead of each atom. This assumes that the mass of each atom type is
+    1. If not, then set the masses keyword to a vector where each element
+    mass[i] is the mass of atoms of type i.
 """
 function msd(filenames...; max_num_frames=100, sorted=false, types=(0,), mol=false, masses=[1,])
     frame = LammpsFiles.read_dump(filenames[1])
