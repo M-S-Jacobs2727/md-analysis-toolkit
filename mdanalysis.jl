8681e285cb192a4a91c18f5830c243d073dc6cb5
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-11-11 11:02:20 -0500

    added exports

--- mdanalysis.jl
+++ mdanalysis.jl
@@ -1,4 +1,5 @@
 module MDAnalysis
+export square_Rg, autocorrelation, mean_square_displacement, radial_dist_func
 include("mean_square_displacement.jl")
 include("radial_dist_func.jl")
 include("square_Rg.jl")
