6c08617ed21c4f1c4b440867d868361a02cef36b
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-11-14 13:48:18 -0500

    packaged the project

--- src/MDAnalysisToolkit.jl
+++ src/MDAnalysisToolkit.jl
@@ -1,4 +1,4 @@
-module MDAnalysis
+module MDAnalysisToolkit
 export square_Rg, autocorrelation, mean_square_displacement, radial_dist_func
 include("mean_square_displacement.jl")
 include("radial_dist_func.jl")
--- src/square_Rg.jl
+++ src/square_Rg.jl
@@ -8,26 +8,28 @@ each snapshot, returning a vector of length equal to the number of files
 passed.
 """
 function square_Rg(filenames...; sorted=false)
+    # Read basic info
     frame = LammpsFiles.read_dump(filenames[1])
     mol_col = findfirst(x->x=="mol", frame.properties)
     coord_cols = [findfirst(x->x==s, frame.properties) for s in ["xu", "yu", "zu"]]
     
-    molecules = frame.atoms[mol_col, : ]
-    nmolecules = length(unique(molecules))
-    natoms_per_mol = [count(x->x==m, molecules) for m in unique(molecules)]
-    inds = [1; cumsum(natoms_per_mol)...]
-    # same as `frame.natoms / nmolecules` for no solvent and monodisperse
+    # Index molecules
+    mol_ids = frame.atoms[mol_col, : ]
+    molecules = sort!(unique(mol_ids))
+    nmolecules = length(molecules)
+    natoms_per_mol = [count(x->x==m, mol_ids) for m in molecules]
+    mol_indices = [1; (cumsum(natoms_per_mol)[1:end-1] .+ 1)...]
     
+    # Get square radius of gyration for each frame
     rg2 = zeros(Float64, length(filenames))
     for (i, filename) in enumerate(filenames)
         frame = LammpsFiles.read_dump(filename)
         molecules = frame.atoms[mol_col, : ]
         coords = frame.atoms[coord_cols, : ]
         if !sorted
-            inds = sortperm(molecules)
-            coords = coords[ : , inds]
+            coords = coords[ : , sortperm(molecules)]
         end
-        coords = [coords[ : , inds[i]:inds[i+1]] for i=1:nmolecules-1]
+        coords = [coords[ : , mol_indices[i]:mol_indices[i+1]] for i=1:nmolecules-1]
 
         centers_of_mass = reshape.(sum.(coords, dims=2), 3)
         for (mol, com, dp) in zip(coords, centers_of_mass, natoms_per_mol)
