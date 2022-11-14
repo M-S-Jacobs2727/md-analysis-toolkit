2124e95926c9e16f9cc66851ab298023a40bbd4b
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-11-14 12:34:19 -0500

    improved msd

--- mean_square_displacement.jl
+++ mean_square_displacement.jl
@@ -3,23 +3,20 @@ using Statistics
 import LammpsFiles
 
 """
-    mean_square_displacement(filenames...; max_num_frames=100, sorted=false, by_type=false, mol=false, masses=nothing)
+    mean_square_displacement(filenames...; max_num_frames=100, types=[], by_mol=false, masses=nothing)
 
 ## Implemented
 Computes the MSD for a collection of LAMMPS dump files. The range of
 increments is from every 1 snapshot to every `max_num_frames`
-snapshots (100 by default). 
+snapshots (100 by default).
 
-If `sorted == true`, then the atoms are assumed to be in the same order
-in each file. If `false` (default), they are sorted by the column `id`.
-
-If `by_type == false` (default), the MSD for each particle is averaged. 
-If `by_type == true`, then each type is independently evaluated, and an 
+If `types` is given, then each type is independently evaluated, and an 
 overall MSD is given as well. Types are only checked for the first dump
 file, so if atoms change type, that will invalidate the calculation.
+By default, the MSD for each particle is averaged.
 
 ## Not implemented
-If `mol` is set to true, then the molecule property must be available in
+If `by_mol` is set to true, then the molecule property must be available in
 the dump file. The center of mass of each molecule will be analyzed 
 instead of each atom. This assumes that the mass of each atom type is 1.
 If not, then the masses keyword should be set to a vector where each 
@@ -27,7 +24,7 @@ element `mass[i]` is the mass of atoms of type `i`.
 
 Note: cannot analyze by type and by molecule at the same time.
 """
-function mean_square_displacement(filenames...; max_num_frames=100, types=[], mol=false, masses=nothing)
+function mean_square_displacement(filenames...; max_num_frames=100, types=nothing, by_mol=false, masses=nothing)
     # Read basic info
     frame = LammpsFiles.read_dump(filenames[1])
     id_col = findfirst(x->x=="id", frame.properties)
@@ -46,14 +43,15 @@ function mean_square_displacement(filenames...; max_num_frames=100, types=[], mo
     end
     
     # Set up types
-    by_type = false
-    if length(types) > 0
-        by_type = true
+    by_type = types !== nothing
+    if by_type
         selections = [
             findall(x->round(x)==t,
                     frame.atoms[type_col, : ][sort_indices])
             for t in types
         ]
+    else
+        types = []
     end
 
     # Read in coordinates
@@ -63,10 +61,10 @@ function mean_square_displacement(filenames...; max_num_frames=100, types=[], mo
     for (i, f) in enumerate(filenames[2:end])
         frame = LammpsFiles.read_dump(f)
         coords = frame.atoms[coord_cols, : ]
-        if !sorted
-            all_coords[ : , : , i+1] = coords[ : , sortperm(frame.atoms[id_col, : ])]
-        else
+        if sorted
             all_coords[ : , : , i+1] = coords
+        else
+            all_coords[ : , : , i+1] = coords[ : , sortperm(frame.atoms[id_col, : ])]
         end
     end
 
