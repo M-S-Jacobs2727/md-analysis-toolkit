39c1b06f234f253a2054ee9057447e52b413316e
M-S-Jacobs2727 <42037940+M-S-Jacobs2727@users.noreply.github.com>
2022-11-15 10:41:35 -0500

    updated computeBond doc

--- src/computeBond.jl
+++ src/computeBond.jl
@@ -5,18 +5,48 @@ using LammpsFiles
 """
     computeBond(datafile::AbstractString, dumpfiles::AbstractString...=nothing; bondtypes=nothing)
 
-Compute the mean, standard deviation, variance, and distribution of bond
-lengths in a LAMMPS data file, optionally including a series of dump files
-for averaging.
+Compute the average, standard deviation, mean-squared average, fluctuations,
+and distribution of bond lengths in a LAMMPS data file, optionally including
+a series of dump files for averaging.
+
+## Positional Arguments
+
+`datafile` (String): Path leading to a LAMMPS data file.
+
+`dumpfiles` (Strings, optional): Paths leading to LAMMPS dump files with all
+the same atoms and bonds as the data file.
+
+## Keyword Arguments
+
+`bondtypes` (Vector of Integers): By default, all bonds are averaged together.
+By specifying a list of bond types, the return values will only account for 
+the types given.
+
+## Return Values
+
+`bonds` (Named Tuple): 
+- `bondlengths` (Vector of Floats): The length of each bond in each frame read.
+To be used for distribution analysis.
+- `average` (Float): The standard mean of the bond lengths.
+- `stddev` (Float): The standard deviation of the bond lengths (normalized
+by the number of bonds).
+- `meansq` (Float): The standard mean of the squared bond lengths.
+- `fluctuation` (Float): The difference between `meansq` and the square of
+`average`.
+
+## TODO
+
+- Change `bondtypes` to `bytype` so that it accounts for each bond type
+separately, like `meanSquareDisplacement`.
+- Implement dump file analysis.
 """
 function computeBond(datafile::AbstractString, dumpfiles::AbstractString...=nothing; atomstyle="full", bondtypes=nothing)
     data = LammpsFiles.readData(datafile, atomstyle=atomstyle)
-    bonds = data.bonds
-    bond_types = data.bond_types
-    coords = data.coords
-    atom_ids = data.atom_ids
-    idtoindex = data.idtoindex
-    bondlengths = hypot.(coords[ : , idtoindex[bonds[1, : ]]] - coords[ : , idtoindex[bonds[2, : ]]])
+    bonds = data.bonds[ : , findall(t->t in bondtypes, data.bond_types)]
+    bondlengths = hypot.(
+        data.coords[ : , data.idtoindex[bonds[1, : ]]] 
+        - data.coords[ : , data.idtoindex[bonds[2, : ]]]
+    )
     average = mean(bondlengths)
     stddev = stdm(bondlengths, average)
     meansq = mean(bondlengths.^2)
